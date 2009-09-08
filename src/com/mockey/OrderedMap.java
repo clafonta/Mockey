111ad4f53f377c0eb69eec74c71f6762fa1d0a3e
neil cronin <neil@neil-cronins-imac.local>
2009-09-07 17:46:17 -0700

    just some cleanup.

--- src/com/mockey/OrderedMap.java
+++ src/com/mockey/OrderedMap.java
@@ -17,7 +17,6 @@ package com.mockey;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -26,13 +25,22 @@ import org.apache.log4j.Logger;
 import com.mockey.model.PersistableItem;
 
 /**
- * Keeps an ordered map of items, order based on 'when defined and added to this object'
+ * Keeps an ordered map of items, with a key of an number based on 'when defined and added to this 
+ * object'.  Sort of like an auto-incrementing column in a db.
+ * <p>
+ * So:
+ * <p>
+ * <code>
+ * OrderedMap<MyObj> myMap = new OrderedMap<MyObj>();<br>
+ * 0 == myMap.save(new MyObj()).getId();<br>
+ * 1 == myMap.save(new MyObj()).getId();<br>
+ * 2 == myMap.save(new MyObj()).getId();<br>
+ * </code>
  * 
  * @author chad.lafontaine
  *
  */
-public class OrderedMap<T extends PersistableItem> extends HashMap<Long, T> 
-	implements Map<Long, T> {
+public class OrderedMap<T extends PersistableItem> extends HashMap<Long, T> implements Map<Long, T> {
 	
 	private static final long serialVersionUID = -1654150132938363942L;
 	private static Logger logger = Logger.getLogger(OrderedMap.class);
@@ -65,17 +73,17 @@ public class OrderedMap<T extends PersistableItem> extends HashMap<Long, T>
 	
     public List<T> getOrderedList(){
 		// Temp
-    	List<Long> orderedListOfKeys = new ArrayList<Long>();
-    	for(Long key : this.keySet()) {
-    		int index = 0;
-    		for (Long current : orderedListOfKeys) {
-    			if(current > key) {
-    				break;
-    			}
-    			index++;
-    		}
-    		orderedListOfKeys.add(index, key);
-    	}
+	    	List<Long> orderedListOfKeys = new ArrayList<Long>();
+	    	for(Long key : this.keySet()) {
+	    		int index = 0;
+	    		for (Long current : orderedListOfKeys) {
+	    			if(current > key) {
+	    				break;
+	    			}
+	    			index++;
+	    		}
+	    		orderedListOfKeys.add(index, key);
+	    	}
 
 		// Ordered key list.
 		List<T> orderedListOfValues = new ArrayList<T>();
