b22b5b0bd0f1b3f45883ce53d5841e3b3692f440
Lorin Kobashigawa-Bates <lorin@Zoe.local>
2009-09-16 11:05:07 -0700

    Added a real command line parser to the Mockey Runner.
    this changes the port argument from -port to --port.

--- src/java/com/mockey/runner/Main.java
+++ src/java/com/mockey/runner/Main.java
@@ -1,5 +1,9 @@
 package com.mockey.runner;
 
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.SimpleJSAP;
 import org.mortbay.jetty.runner.Runner;
 
 import java.io.File;
@@ -8,20 +12,31 @@ public class Main {
 
 
     public static void main(String args[]) throws Exception {
-        String port = "8080";
-
-        if (args!=null) { // running in-proc in win32 passes NULL to the jvm
-        	for (int i = 0; i < args.length; i++) {
-        		if ("--port".equals(args[i])) {
-        			port = args[++i];
-        		}
-        	}
-        }
+        if(args == null) args = new String[0];
         
+
+        // Initialize the argument parser
+        SimpleJSAP jsap = new SimpleJSAP("java -jar Mockey.jar","Starts a Jetty server running Mockey");
+        jsap.registerParameter(new FlaggedOption("port", JSAP.INTEGER_PARSER,"8080",JSAP.NOT_REQUIRED,'p',"port", "port to run Jetty on"));
+
+
+        // parse the command line options
+        JSAPResult config = jsap.parse(args);
+
+        // Bail out if they asked for the --help
+        if ( jsap.messagePrinted() ) System.exit( 1 );
+
+
+        // Construct the new arguments for jetty-runner
+        String port = String.valueOf(config.getInt("port"));
         String[] argv = {"--port", port, "Mockey.war"};
 
+
+        // Start the default browser in 10 seconds
         new Thread(new BrowserThread("http://127.0.0.1:", port, 10)).start();
 
+
+        // start Jetty
         Runner runner = new Runner();
         runner.configure(argv);
         runner.run();
