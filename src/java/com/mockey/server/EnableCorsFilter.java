c21105695bfc36404c6ad12bac84d6d7746c131c
Chad Lafontaine <chad.lafontaine@gmail.com>
2013-01-10 08:06:19 -0800

    Added Access-Control-Allow-Origin to response headers so people don't have to setup a reverse proxy

---
+++ src/java/com/mockey/server/EnableCorsFilter.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of Mockey, a tool for testing application 
+ * interactions over HTTP, with a focus on testing web services, 
+ * specifically web applications that consume XML, JSON, and HTML.
+ *  
+ * Copyright (C) 2009-2013  Authors:
+ * 
+ * chad.lafontaine (chad.lafontaine AT gmail DOT com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+package com.mockey.server;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * If the response from Mockey includes the header
+ * 
+ * <pre>
+ * Access-Control-Allow-Origin: *
+ * </pre>
+ * 
+ * then users can do Ajax request from any domain to Mockey without the need to
+ * set up a reverse proxy. See http://enable-cors.org
+ * 
+ * @author clafonta
+ * 
+ */
+public class EnableCorsFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		//
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws ServletException, java.io.IOException {
+		HttpServletResponse response = (HttpServletResponse) resp;
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		// response.addHeader("Access-Control-Allow-Credentials", "true");
+
+		chain.doFilter(req, resp);
+	}
+
+	@Override
+	public void destroy() {
+		//
+	}
+}
