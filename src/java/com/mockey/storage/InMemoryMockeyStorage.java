f0d83a4b34e66d7687a99ff0bf1f00a20970a2bb
mohan <mohanraoa@virtusa.com>
2019-12-28 16:03:21 +0530

    Fix for the issue, https://github.com/clafonta/Mockey/issues/109.

--- src/java/com/mockey/storage/InMemoryMockeyStorage.java
+++ src/java/com/mockey/storage/InMemoryMockeyStorage.java
@@ -28,6 +28,8 @@
 package com.mockey.storage;
 
 import java.net.MalformedURLException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
@@ -245,21 +247,29 @@ public class InMemoryMockeyStorage implements IMockeyStorage {
 	 * @return service if url pattern matches
 	 */
 	private Service findServiceBasedOnUrlPattern(String url, Service serviceToEvaluate) {
-		Url fullUrl = new Url(serviceToEvaluate.getUrl());
 		Service foundService = null;
+		String decodedUrl = null;
+		try {
+			decodedUrl = URLDecoder.decode(url, "UTF-8");
+		}catch(Exception e) {
+			System.out.println(e);
+			return foundService;
+		}
+		Url fullUrl = new Url(serviceToEvaluate.getUrl());
+		
 		// EXAMPLE: "http://example.com/hotels/{hotel}/bookings/{booking}"
 		UriTemplate template = new UriTemplate(fullUrl.getFullUrl());
 
 		// EXAMPLE: "http://example.com/hotels/1/bookings/42"
 		@SuppressWarnings("rawtypes")
-		Map results = template.match(url);
-		if (results.size() > 0) {
+		Map results = template.match(decodedUrl);
+		if (results.size() > 0 && template.matches(decodedUrl)) {
 			// Possible match
 			foundService = serviceToEvaluate;
 		} else {
 
 			// OK, not found based on template URL.
-			if (fullUrl.getFullUrl().equalsIgnoreCase(url)) {
+			if (fullUrl.getFullUrl().equalsIgnoreCase(decodedUrl)) {
 				foundService = serviceToEvaluate;
 			} else {
 				// Let's look at secondary list of real URLs
@@ -275,9 +285,8 @@ public class InMemoryMockeyStorage implements IMockeyStorage {
 					// be
 					// matched against the url of the request instead.
 					template = new UriTemplate(altUrl.getFullUrl());
-					results = template.match(url);
-
-					if (results.size() > 0) {
+					results = template.match(decodedUrl);
+					if (results.size() > 0  && template.matches(decodedUrl)) {
 						// Possible match
 						foundService = serviceToEvaluate;
 						break;
